6248f09cf1764145ff56d6ead7dca8f7885d0230
Olivier DOSSMANN <olivier@dossmann.net>
2010-11-06 19:55:16 +0100

    Ajout des réponses aux questions à la fin de l'exécution du programme

--- kezako.py
+++ kezako.py
@@ -25,6 +25,8 @@ separateur = '###'        # Séparateur contenu dans le fichier source
 nbre_questions = 3        # Nombre de questions posées
 temps_pause = 2           # Temps de pause entre chaque question (en secondes)
 lotterie = []             # Les numéros des questions non-posées
+tirage = []               # Liste des numéros tirés
+reponses = []             # Ensemble des réponses correspondantes
 
 # Deboggage
 deboggage = True
@@ -53,6 +55,9 @@ for i in range(0,longueur_questions - 1):
   # Ajout du numéro dans la lotterie
   lotterie.append(i)
 
+# Affichage des questions
+print(" QUESTIONS ".center(80, '='))
+
 # Sortie d'un nombre défini de questions
 for i in range(0,nbre_questions):
   # Recalcul de la capacité, en nombre, de la lotterie
@@ -64,6 +69,9 @@ for i in range(0,nbre_questions):
   # Récupération du contenu du numéro
   nbr_aleatoire = lotterie[pioche_aleatoire]
 
+  # Ajout du numéro dans le tirage
+  tirage.append(nbr_aleatoire)
+
   # Affichage de la question
   print("Question %d : %s" % (nbr_aleatoire, questions[nbr_aleatoire].split(separateur)[0]))
 
@@ -72,3 +80,19 @@ for i in range(0,nbre_questions):
 
   # Petite pause
   pause(temps_pause)
+
+# Recherche des réponses
+for i in tirage:
+  reponses.append(questions[i].split(separateur)[1])
+
+# Séparation avec la suite du programme
+print("".center(80, '-'))
+
+# Attente d'interaction avec l'utilisateur
+input("Appuie sur Entrée dès que tu voudras afficher les réponses correspondantes.")
+
+# Affichage des réponses
+print(" REPONSES ".center(80, '='))
+for nbre,reponse in zip(tirage,reponses):
+  print("Question {} : {}".format(nbre, reponse.replace('\n', '')))
+
