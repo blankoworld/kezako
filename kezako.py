c19343c7add5d64c9a718411d1236332e23e5b18
Olivier DOSSMANN <olivier@dossmann.net>
2010-11-06 18:35:42 +0100

    Affichage de plusieurs questions d'un coup avec un temps de pause entre chacun

--- kezako.py
+++ kezako.py
@@ -14,13 +14,16 @@
 # La licence de ce logiciel évoluera a posteriori.
 
 import random
+from time import sleep as pause
 from fichier import Fichier
 
 # Déclaration des variables
-questions = []
-longueur_questions = 0
-source = 'questions.txt'
-separateur = '###'
+questions = []            # Les questions
+longueur_questions = 0    # Nombre de questions dans KEZAKO
+source = 'questions.txt'  # Fichier où trouver les questions/réponses
+separateur = '###'        # Séparateur contenu dans le fichier source
+nbre_questions = 3        # Nombre de questions posées
+temps_pause = 2           # Temps de pause entre chaque question (en secondes)
 
 # Deboggage
 deboggage = False
@@ -28,7 +31,9 @@ deboggage = False
 # Fonctions
 # Fonction de deboggage
 def debug(texte):
-  """Affiche la chaîne de caractère reçue si le programme est en mode deboggage"""
+  """Affiche la chaîne de caractère reçue si le programme est en mode 
+  deboggage
+  """
   if deboggage == True:
     print(str(texte))
 
@@ -42,8 +47,13 @@ questions = fichier.contenu
 # Lecture des questions
 longueur_questions = len(questions)
 
-# Choix aléatoire d'une question
-nbr_aleatoire = random.randint(0,longueur_questions - 1)
+# Sortie d'un nombre défini de questions
+for i in range(0,nbre_questions):
+  # Choix aléatoire d'une question
+  nbr_aleatoire = random.randint(0,longueur_questions - 1)
 
-# Affichage de la question
-print("Question %d : %s" % (nbr_aleatoire, questions[nbr_aleatoire].split(separateur)[0]))
+  # Affichage de la question
+  print("Question %d : %s" % (nbr_aleatoire, questions[nbr_aleatoire].split(separateur)[0]))
+
+  # Petite pause
+  pause(temps_pause)
