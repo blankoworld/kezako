1dd18b277c1b221c7f0c69de7f12aeb256417e16
Olivier DOSSMANN <olivier@dossmann.net>
2010-11-06 19:20:39 +0100

    Amélioration : n'affiche PAS deux fois la même question

--- kezako.py
+++ kezako.py
@@ -18,15 +18,16 @@ from time import sleep as pause
 from fichier import Fichier
 
 # Déclaration des variables
-questions = []            # Les questions
+questions = []            # Les questions initiales
 longueur_questions = 0    # Nombre de questions dans KEZAKO
 source = 'questions.txt'  # Fichier où trouver les questions/réponses
 separateur = '###'        # Séparateur contenu dans le fichier source
 nbre_questions = 3        # Nombre de questions posées
 temps_pause = 2           # Temps de pause entre chaque question (en secondes)
+lotterie = []             # Les numéros des questions non-posées
 
 # Deboggage
-deboggage = False
+deboggage = True
 
 # Fonctions
 # Fonction de deboggage
@@ -47,13 +48,27 @@ questions = fichier.contenu
 # Lecture des questions
 longueur_questions = len(questions)
 
+# Création d'une lotterie d'éléments contenant les questions non-posées
+for i in range(0,longueur_questions - 1):
+  # Ajout du numéro dans la lotterie
+  lotterie.append(i)
+
 # Sortie d'un nombre défini de questions
 for i in range(0,nbre_questions):
-  # Choix aléatoire d'une question
-  nbr_aleatoire = random.randint(0,longueur_questions - 1)
+  # Recalcul de la capacité, en nombre, de la lotterie
+  capacite_lotterie = len(lotterie)
+
+  # Choix aléatoire d'un numéro dans la lotterie
+  pioche_aleatoire = random.randint(0,capacite_lotterie - 1)
+
+  # Récupération du contenu du numéro
+  nbr_aleatoire = lotterie[pioche_aleatoire]
 
   # Affichage de la question
   print("Question %d : %s" % (nbr_aleatoire, questions[nbr_aleatoire].split(separateur)[0]))
+
+  # Suppression de l'élément dans la lotterie
+  lotterie.remove(nbr_aleatoire)
 
   # Petite pause
   pause(temps_pause)
